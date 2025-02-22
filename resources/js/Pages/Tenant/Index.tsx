dc8824112bd68f45561461d9c90b4789e26e9b22
Isaac Hatilima <isaachatilima@gmail.com>
2025-02-22 16:30:50 +0100

    Added badge to tenant status

--- resources/js/Pages/Tenant/Index.tsx
+++ resources/js/Pages/Tenant/Index.tsx
@@ -2,6 +2,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { PaginatedTenants, Tenant } from '@/types/tenant';
 import { Head, Link, router, useForm, usePage } from '@inertiajs/react';
 import {
+    Badge,
     Card,
     Group,
     Input,
@@ -30,7 +31,15 @@ function Index() {
             </Table.Td>
             <Table.Td>{tenant.name}</Table.Td>
             <Table.Td>{tenant.domain.domain}</Table.Td>
-            <Table.Td>{tenant.status}</Table.Td>
+            <Table.Td>
+                <Badge
+                    color={tenant.status == 'active' ? 'green' : 'red'}
+                    variant="outline"
+                    size="sm"
+                >
+                    {tenant.status}
+                </Badge>
+            </Table.Td>
             <Table.Td>{tenant.contact_name}</Table.Td>
             <Table.Td>{formatDate(tenant.created_at)}</Table.Td>
         </Table.Tr>
@@ -72,181 +81,161 @@ function Index() {
         <AuthenticatedLayout>
             <Head title="Tenants" />
 
-            <div className="">
-                <Card shadow="sm" padding="lg" radius="md">
-                    <div>
-                        <Table striped highlightOnHover>
-                            <Table.Thead>
-                                <Table.Tr>
-                                    <Table.Th>Tenant Number</Table.Th>
-                                    <Table.Th>Name</Table.Th>
-                                    <Table.Th>Domain</Table.Th>
-                                    <Table.Th>Status</Table.Th>
-                                    <Table.Th>Contact</Table.Th>
-                                    <Table.Th>Created</Table.Th>
-                                </Table.Tr>
-                                <Table.Tr>
-                                    <Table.Th>
-                                        <TextInput
-                                            id="tenant_number"
-                                            name="tenant_number"
-                                            placeholder="Tenant Number"
-                                            value={data.tenant_number}
-                                            onChange={(e) =>
-                                                setData(
-                                                    'tenant_number',
-                                                    e.target.value,
-                                                )
-                                            }
-                                        />
-                                    </Table.Th>
-                                    <Table.Th>
-                                        <TextInput
-                                            id="name"
-                                            name="name"
-                                            placeholder="Tenant Name"
-                                            value={data.name}
-                                            onChange={(e) =>
-                                                setData('name', e.target.value)
-                                            }
-                                        />
-                                    </Table.Th>
-                                    <Table.Th>
-                                        <TextInput
-                                            id="domain"
-                                            name="domain"
-                                            placeholder="Domain"
-                                            value={data.domain}
-                                            onChange={(e) =>
-                                                setData(
-                                                    'domain',
-                                                    e.target.value,
-                                                )
-                                            }
-                                        />
-                                    </Table.Th>
-                                    <Table.Th>
-                                        <Input
-                                            id="status"
-                                            name="status"
-                                            component="select"
-                                            value={data.status}
-                                            onChange={(e) =>
-                                                setData(
-                                                    'status',
-                                                    e.target.value,
-                                                )
-                                            }
-                                        >
-                                            <option value="">All</option>
-                                            <option value="active">
-                                                Active
-                                            </option>
-                                            <option value="in-active">
-                                                In-Active
-                                            </option>
-                                        </Input>
-                                    </Table.Th>
-                                    <Table.Th>
-                                        <TextInput
-                                            id="contact_name"
-                                            name="contact_name"
-                                            placeholder="Contact Name"
-                                            value={data.contact_name}
-                                            onChange={(e) =>
-                                                setData(
-                                                    'contact_name',
-                                                    e.target.value,
-                                                )
-                                            }
-                                        />
-                                    </Table.Th>
-                                    <Table.Th>
-                                        <Input
-                                            id="sorting"
-                                            name="sorting"
-                                            component="select"
-                                            value={data.sorting}
-                                            onChange={(e) =>
-                                                setData(
-                                                    'sorting',
-                                                    e.target.value,
-                                                )
-                                            }
-                                        >
-                                            <option value="ascending">
-                                                Ascending
-                                            </option>
-                                            <option value="descending">
-                                                Descending
-                                            </option>
-                                        </Input>
-                                    </Table.Th>
-                                </Table.Tr>
-                            </Table.Thead>
-                            <Table.Tbody>{rows}</Table.Tbody>
-                        </Table>
-                    </div>
+            <Card shadow="sm" padding="lg" radius="md">
+                <div>
+                    <Table striped highlightOnHover>
+                        <Table.Thead>
+                            <Table.Tr>
+                                <Table.Th>Tenant Number</Table.Th>
+                                <Table.Th>Name</Table.Th>
+                                <Table.Th>Domain</Table.Th>
+                                <Table.Th>Status</Table.Th>
+                                <Table.Th>Contact</Table.Th>
+                                <Table.Th>Created</Table.Th>
+                            </Table.Tr>
+                            <Table.Tr>
+                                <Table.Th>
+                                    <TextInput
+                                        id="tenant_number"
+                                        name="tenant_number"
+                                        placeholder="Tenant Number"
+                                        value={data.tenant_number}
+                                        onChange={(e) =>
+                                            setData(
+                                                'tenant_number',
+                                                e.target.value,
+                                            )
+                                        }
+                                    />
+                                </Table.Th>
+                                <Table.Th>
+                                    <TextInput
+                                        id="name"
+                                        name="name"
+                                        placeholder="Tenant Name"
+                                        value={data.name}
+                                        onChange={(e) =>
+                                            setData('name', e.target.value)
+                                        }
+                                    />
+                                </Table.Th>
+                                <Table.Th>
+                                    <TextInput
+                                        id="domain"
+                                        name="domain"
+                                        placeholder="Domain"
+                                        value={data.domain}
+                                        onChange={(e) =>
+                                            setData('domain', e.target.value)
+                                        }
+                                    />
+                                </Table.Th>
+                                <Table.Th>
+                                    <Input
+                                        id="status"
+                                        name="status"
+                                        component="select"
+                                        value={data.status}
+                                        onChange={(e) =>
+                                            setData('status', e.target.value)
+                                        }
+                                    >
+                                        <option value="">All</option>
+                                        <option value="active">Active</option>
+                                        <option value="in-active">
+                                            In-Active
+                                        </option>
+                                    </Input>
+                                </Table.Th>
+                                <Table.Th>
+                                    <TextInput
+                                        id="contact_name"
+                                        name="contact_name"
+                                        placeholder="Contact Name"
+                                        value={data.contact_name}
+                                        onChange={(e) =>
+                                            setData(
+                                                'contact_name',
+                                                e.target.value,
+                                            )
+                                        }
+                                    />
+                                </Table.Th>
+                                <Table.Th>
+                                    <Input
+                                        id="sorting"
+                                        name="sorting"
+                                        component="select"
+                                        value={data.sorting}
+                                        onChange={(e) =>
+                                            setData('sorting', e.target.value)
+                                        }
+                                    >
+                                        <option value="ascending">
+                                            Ascending
+                                        </option>
+                                        <option value="descending">
+                                            Descending
+                                        </option>
+                                    </Input>
+                                </Table.Th>
+                            </Table.Tr>
+                        </Table.Thead>
+                        <Table.Tbody>{rows}</Table.Tbody>
+                    </Table>
+                </div>
 
-                    <div className="mt-4 flex justify-end">
-                        <Pagination.Root
-                            total={paginatedTenants.last_page}
-                            value={paginatedTenants.current_page}
-                            getItemProps={(page) => ({
-                                href: paginatedTenants.links[page]?.url,
-                                onClick: () => {
-                                    if (paginatedTenants.links[page]?.url) {
-                                        router.visit(
-                                            page == 1
-                                                ? paginatedTenants.path
-                                                : paginatedTenants.links[page]
-                                                      .url,
-                                        );
-                                    }
-                                },
-                            })}
-                        >
-                            <Group gap={5} justify="center">
-                                <Pagination.First
-                                    onClick={() =>
-                                        router.visit(paginatedTenants.path)
-                                    }
-                                />
-                                <Pagination.Previous
-                                    onClick={() =>
-                                        router.visit(
-                                            paginatedTenants.prev_page_url,
-                                        )
-                                    }
-                                />
+                <div className="mt-4 flex justify-end">
+                    <Pagination.Root
+                        total={paginatedTenants.last_page}
+                        value={paginatedTenants.current_page}
+                        getItemProps={(page) => ({
+                            href: paginatedTenants.links[page]?.url,
+                            onClick: () => {
+                                if (paginatedTenants.links[page]?.url) {
+                                    router.visit(
+                                        page == 1
+                                            ? paginatedTenants.path
+                                            : paginatedTenants.links[page].url,
+                                    );
+                                }
+                            },
+                        })}
+                    >
+                        <Group gap={5} justify="center">
+                            <Pagination.First
+                                onClick={() =>
+                                    router.visit(paginatedTenants.path)
+                                }
+                            />
+                            <Pagination.Previous
+                                onClick={() =>
+                                    router.visit(paginatedTenants.prev_page_url)
+                                }
+                            />
 
-                                <Pagination.Items />
+                            <Pagination.Items />
 
-                                <Pagination.Next
-                                    onClick={() =>
-                                        router.visit(
-                                            paginatedTenants.next_page_url,
-                                        )
-                                    }
-                                />
-                                <Pagination.Last
-                                    onClick={() =>
-                                        router.visit(
-                                            paginatedTenants.last_page_url,
-                                        )
-                                    }
-                                />
-                            </Group>
-                        </Pagination.Root>
-                    </div>
-                    <div className="mt-4 flex justify-end">
-                        <p className="text-sm font-thin text-gray-400">
-                            {paginatedTenants.from} to {paginatedTenants.to} of{' '}
-                            {paginatedTenants.total}
-                        </p>
-                    </div>
-                </Card>
-            </div>
+                            <Pagination.Next
+                                onClick={() =>
+                                    router.visit(paginatedTenants.next_page_url)
+                                }
+                            />
+                            <Pagination.Last
+                                onClick={() =>
+                                    router.visit(paginatedTenants.last_page_url)
+                                }
+                            />
+                        </Group>
+                    </Pagination.Root>
+                </div>
+                <div className="mt-4 flex justify-end">
+                    <p className="text-sm font-thin text-gray-400">
+                        {paginatedTenants.from} to {paginatedTenants.to} of{' '}
+                        {paginatedTenants.total}
+                    </p>
+                </div>
+            </Card>
         </AuthenticatedLayout>
     );
 }
--- resources/js/Pages/Tenant/TenantDetail.tsx
+++ resources/js/Pages/Tenant/TenantDetail.tsx
@@ -1,13 +1,17 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Tenant } from '@/types/tenant';
 import { Head, usePage } from '@inertiajs/react';
+import { Card } from '@mantine/core';
 
 function TenantDetail() {
     const tenant: Tenant = usePage().props.tenant;
     return (
         <AuthenticatedLayout>
             <Head title="Tenants" />
-            {tenant.name}
+
+            <Card shadow="sm" padding="lg" radius="md">
+                {tenant.name}
+            </Card>
         </AuthenticatedLayout>
     );
 }
