748974d4b4cfba44026861beaa87de73d7d37c1f
Isaac Hatilima <isaachatilima@gmail.com>
2025-02-23 14:22:27 +0100

    added tenants details page and updated npm

--- resources/js/Pages/Tenant/TenantDetail.tsx
+++ resources/js/Pages/Tenant/TenantDetail.tsx
@@ -1,7 +1,13 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Tenant } from '@/types/tenant';
 import { Head, usePage } from '@inertiajs/react';
-import { Card } from '@mantine/core';
+import { Card, Tabs } from '@mantine/core';
+import {
+    MdOutlineContactMail,
+    MdOutlinePayments,
+    MdOutlinePeopleAlt,
+    MdOutlineStickyNote2,
+} from 'react-icons/md';
 
 function TenantDetail() {
     const tenant: Tenant = usePage().props.tenant;
@@ -10,7 +16,47 @@ function TenantDetail() {
             <Head title="Tenants" />
 
             <Card shadow="sm" padding="lg" radius="md">
-                {tenant.name}
+                <Tabs variant="outline" defaultValue="gallery">
+                    <Tabs.List>
+                        <Tabs.Tab
+                            value="gallery"
+                            leftSection={<MdOutlineContactMail size={15} />}
+                        >
+                            Tenant Details
+                        </Tabs.Tab>
+                        <Tabs.Tab
+                            value="messages"
+                            leftSection={<MdOutlinePeopleAlt size={15} />}
+                        >
+                            Users
+                        </Tabs.Tab>
+                        <Tabs.Tab
+                            value="settings"
+                            leftSection={<MdOutlinePayments size={15} />}
+                        >
+                            Payments
+                        </Tabs.Tab>
+                        <Tabs.Tab
+                            value="licenses"
+                            leftSection={<MdOutlineStickyNote2 size={15} />}
+                        >
+                            Licenses
+                        </Tabs.Tab>
+                    </Tabs.List>
+
+                    <Tabs.Panel value="gallery">{tenant.name}</Tabs.Panel>
+
+                    <Tabs.Panel value="messages">
+                        Messages tab content
+                    </Tabs.Panel>
+
+                    <Tabs.Panel value="settings">
+                        Settings tab content
+                    </Tabs.Panel>
+                    <Tabs.Panel value="licenses">
+                        Licenses tab content
+                    </Tabs.Panel>
+                </Tabs>
             </Card>
         </AuthenticatedLayout>
     );
