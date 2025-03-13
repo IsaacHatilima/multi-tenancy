900abd8a04efbc0c93792baea43c48de87a2cdca
Isaac Hatilima <isaachatilima@gmail.com>
2025-03-13 13:52:57 +0100

    Refactored routes

--- routes/guest-common.php
+++ routes/guest-common.php
@@ -1,60 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\GoogleAuthController;
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::middleware(['guest'])->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
-
-    Route::post('register', [RegisteredUserController::class, 'store'])
-        ->name('register.store');
-
-    Route::get('/google/redirect', [GoogleAuthController::class, 'redirectToGoogle'])
-        ->name('google.redirect');
-
-    Route::get('/google/callback', [GoogleAuthController::class, 'handleGoogleCallback'])
-        ->name('google.callback');
-
-    Route::get('/', [LoginController::class, 'create'])
-        ->name('login');
-
-    Route::post('/', [LoginController::class, 'authenticate'])
-        ->name('login.post');
-
-    Route::post('/auth-check', [LoginController::class, 'auth_check'])
-        ->name('login.auth.check');
-
-    Route::get('/two-factor', [LoginController::class, 'email_two_factor'])
-        ->name('login.email.two.factor');
-
-    Route::post('/two-factor', [LoginController::class, 'email_two_factor_authenticate'])
-        ->name('login.email.two.post');
-
-    Route::get('/new-two-factor', [LoginController::class, 'request_new_code'])
-        ->name('new.two.factor.code');
-
-    Route::get('/two-factor-challenge', function () {
-        return Inertia::render('Auth/TwoFactorChallenge');
-    })->name('two-factor.login');
-
-    Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-        ->name('password.request');
-
-    Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
+    require __DIR__.'/guest/index.php';
 });
 
 Route::get('verify-email/{id}', VerifyEmailController::class)
--- routes/protected-common.php
+++ routes/protected-common.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\LogoutController;
-use App\Http\Controllers\Auth\SecurityController;
-use App\Http\Controllers\ProfileController;
 use Inertia\Inertia;
 
 Route::middleware('auth')->group(function () {
@@ -11,21 +9,13 @@ Route::middleware('auth')->group(function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    require __DIR__.'/protected/security.php';
 
-    require __DIR__.'/auth/security.php';
-
-    Route::get('/security', [SecurityController::class, 'edit'])->name('security.edit');
-    Route::put('/security', [SecurityController::class, 'copy_recovery_codes'])->name('security.put');
-    Route::put('/password', [SecurityController::class, 'update'])->name('password.update');
-    Route::put('/manage-email-two-factor/{type}', [SecurityController::class, 'email_two_factor'])->name('email.fa');
+    require __DIR__.'/protected/profile.php';
 
     Route::post('email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
         ->middleware('throttle:6,1')
         ->name('verification.send');
 
-    // Using /logout conflicts with fortify route preventing tenant from login out
     Route::post('/sign-out', [LogoutController::class, 'destroy'])->name('sign.out');
 });
