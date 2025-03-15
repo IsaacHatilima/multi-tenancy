fdb2938fd073caddc2804677dca5e7291e23ca8a
Isaac Hatilima <isaachatilima@gmail.com>
2025-03-15 09:58:27 +0100

    Refactored Enable Fortify 2FA

--- tests/Feature/Auth/TwoFactorActivationTest.php
+++ tests/Feature/Auth/TwoFactorActivationTest.php
@@ -1,24 +1,44 @@
 <?php
 
 use App\Models\User;
+use Inertia\Testing\AssertableInertia as Assert;
 
 test('user can enable 2FA', function () {
     $user = User::factory()->create([
         'password' => Hash::make('Password1#'),
     ]);
 
-    $this->actingAs($user)->post('/user/confirm-password', [
-        'password' => 'Password1#',
-    ]);
+    $this->get(route('login'));
 
-    $response = $this->actingAs($user)->post('/user/two-factor-authentication');
+    $this
+        ->followingRedirects()
+        ->post(route('login.post'), [
+            'email' => $user->email,
+            'password' => 'Password1#',
+        ])
+        ->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Dashboard')
+        );
 
-    $user->refresh();
+    $this->get(route('security.edit'))
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Profile/Security')
+            ->where('errors', [])
+        );
 
-    $response->assertStatus(302);
-    $this->assertNotNull($user->two_factor_secret);
-    $this->assertNotNull($user->two_factor_recovery_codes);
-    $this->assertNull($user->two_factor_confirmed_at);
+    $this
+        ->followingRedirects()
+        ->put(route('enable.fortify'), [
+            'current_password' => 'Password1#',
+        ])
+        ->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Profile/Security')
+            ->whereNot('auth.user.two_factor_secret', null)
+            ->whereNot('auth.user.two_factor_recovery_codes', null)
+            ->where('auth.user.two_factor_confirmed_at', null)
+        );
 });
 
 test('user cannot enable 2FA with wrong password', function () {
@@ -26,17 +46,34 @@ test('user cannot enable 2FA with wrong password', function () {
         'password' => Hash::make('Password1#'),
     ]);
 
-    $response = $this->actingAs($user)->post('/user/confirm-password', [
-        'password' => 'Password12#',
-    ]);
+    $this->get(route('login'));
 
-    $this->assertEquals(session('errors')->get('password')[0], 'The provided password was incorrect.');
+    $this
+        ->followingRedirects()
+        ->post(route('login.post'), [
+            'email' => $user->email,
+            'password' => 'Password1#',
+        ])
+        ->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Dashboard')
+        );
 
-    $user->refresh();
+    $this->get(route('security.edit'))
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Profile/Security')
+            ->where('errors', [])
+        );
 
-    $response->assertStatus(302);
-
-    $this->assertNull($user->two_factor_secret);
-    $this->assertNull($user->two_factor_recovery_codes);
-    $this->assertNull($user->two_factor_confirmed_at);
+    $this
+        ->followingRedirects()
+        ->put(route('enable.fortify'), [
+            'current_password' => 'Password12h#',
+        ])
+        ->assertOk()
+        ->assertInertia(fn (Assert $page) => $page
+            ->component('Profile/Security')
+            ->has('errors')
+            ->where('errors.current_password', 'Current password is incorrect.')
+        );
 });
